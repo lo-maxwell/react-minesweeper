43dbd81976d4e41ba8432fdf80892a07f2cdc2de
Maxwell Lo <lo.maxwell8240@gmail.com>
2024-03-18 23:03:35 -0700

    Add flagging, color scheme changes, win/loss details

--- Minesweeper-Blog.md
+++ Minesweeper-Blog.md
@@ -21,7 +21,21 @@
   * But the bomb rng is a bit skewed, all the bombs are placed towards the top. Check to make sure it's an even chance.
     * Fixed by changing the boundaries from squares.length (which was originally working but we made it 2d) to width * height
   * Board and bomb placement is now fixed
-  * Finished Revealing tiles and recursive reveal
+
+### Finished Revealing tiles and recursive reveal
+
+
+### Add Game Component, restart button
+
+## Day 4
+
+### Added win/loss details
+
+### Added flagging and flag mode
+
+### Changed color scheme to better differentiate hidden, revealed, and flagged tiles
+
+  * Uses React.syntheticEvent and e.preventDefault() to get the right click flag to work
 
 
 ## TODO
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,97 @@
 'use client'
 // Allows useState and onClick, which are client side hooks
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 type SquareType = [string, boolean, number]
 type OnSquareClickHandler = () => void;
+type OnSquareRightClickHandler = (e: React.SyntheticEvent) => void;
 
 const DEFAULT_BOARD_SIZE = 10;
 
-function Square({ state, isBomb, value, onSquareClick}: {state: string, isBomb: boolean, value: number, onSquareClick: OnSquareClickHandler}) {
+function Square({ state, isBomb, value, onSquareLeftClick, onSquareRightClick}: {state: string, isBomb: boolean, value: number, onSquareLeftClick: OnSquareClickHandler, onSquareRightClick: OnSquareRightClickHandler}) {
   if (state === "Hidden") {
-    return (<button className="square" onClick={onSquareClick}></button>);
+    return (<button className="square-hidden" onClick={onSquareLeftClick} onContextMenu={onSquareRightClick}></button>);
+  } else if (state === "Flagged") {
+    return (<button className="square-flag" onClick={onSquareLeftClick} onContextMenu={onSquareRightClick}>Flag</button>);
   }
   if (isBomb) {
-    return (<button className="bomb"> Boom </button>);
+    return (<button className="square-bomb"> Boom </button>);
   } else {
-    return (<button className="square"> {value} </button>);
+    if (value === 0) {
+      return (<button className="square-revealed"></button>);
+    } else {
+      return (<button className="square-revealed"> {value} </button>);
+    }
   }
 }
 
-export default function Board() {
-  const [width, setWidth] = useState(10);
-  const [height, setHeight] = useState(10);
-  const [numBombs, setNumBombs] = useState(20);
+function Board({width, height, numBombs, squares, handleLeftClick, handleRightClick}: {width: number, height: number, numBombs: number, squares: Array<Array<SquareType>>, handleLeftClick: Function, handleRightClick: Function}) {
+  // const [width, setWidth] = useState(10);
+  // const [height, setHeight] = useState(10);
+  // const [numBombs, setNumBombs] = useState(20);
 
-  const generateSquare = (): SquareType =>  ['', false, -2];
-  // const initialSquares = new Array(width * height).fill(0).map(e => ['', false, 0] as SquareType)
-  const generateRow = (): Array<SquareType> => Array(width).fill(0).map(generateSquare);
-  const initialSquares = new Array(height).fill(0).map(generateRow);
+  
   
   // console.log(initialSquares)
-  const [squares, setSquares] = useState<Array<Array<SquareType>>>(initialSquares);
+  // const [squares, setSquares] = useState<Array<Array<SquareType>>>(generateBombs(initialSquares, numBombs));
 
-  useEffect(() => {
-    setSquares(generateBombs(initialSquares, 10));
-  }, []);
+  // useEffect(() => {
+  //   setSquares(generateBombs(initialSquares, numBombs));
+  // }, []);
 
   const rows = Array.from({length: height}, (_, index) => index);
   const cols = Array.from({length: width}, (_, index) => index);
 
+  function renderBoard() {
+    return (
+      <>
+      {rows.map((row) => (
+        <div className="board-row" key={row}>
+          {cols.map((col) => {
+            const index = row * width + col;
+            return (<Square 
+              key = {index}
+              state={squares[row][col][0]} 
+              isBomb={squares[row][col][1]}
+              value={squares[row][col][2]}
+              onSquareLeftClick={() => handleLeftClick(row, col)}
+              onSquareRightClick={(e) => handleRightClick(e, row, col)}
+              />);
+        })}
+        </div>
+      ))}
+      </>
+    )
+  }
+   
+
+  return (
+    <>
+    {renderBoard()}
+    </>
+  );
+}
+
+export default function Game() {
+  const [width, setWidth] = useState(10);
+  const [height, setHeight] = useState(10);
+  const [numBombs, setNumBombs] = useState(5);
+  const generateSquare = (): SquareType =>  ['', false, -2];
+  // const initialSquares = new Array(width * height).fill(0).map(e => ['', false, 0] as SquareType)
+  const generateRow = (): Array<SquareType> => Array(width).fill(0).map(generateSquare);
+  const initialSquares = new Array(height).fill(0).map(generateRow);
+  const [squares, setSquares] = useState<Array<Array<SquareType>>>(generateBombs(initialSquares, numBombs));
+  const rows = Array.from({length: height}, (_, index) => index);
+  const cols = Array.from({length: width}, (_, index) => index);
+  const [gameIsOver, setGameIsOver] = useState(false);
+  const [flagMode, setFlagMode] = useState(false);
+  const [gameStatus, setGameStatus] = useState("");
+  const [squaresRevealed, setSquaresRevealed] = useState(0);
+  let squaresRevealedCounter = 0;
+
   function generateBombs(squares: Array<Array<SquareType>>, bombs: number) : Array<Array<SquareType>> {
     const nextSquares = squares.slice();
-    
     let bombsPlaced = 0;
     //plant bombs
     for (let i = 0; i < height; i++) {
@@ -90,54 +140,103 @@ export default function Board() {
     return nextSquares;
   }
 
-  function revealSquare (currentSquares: Array<Array<SquareType>>, row: number, col: number) {
-    var nextSquares = currentSquares.slice();
-    if (nextSquares[row][col][0] === "Hidden") {
-      nextSquares[row][col][0] = "Revealed";
-      if (nextSquares[row][col][2] === 0) {
-        for (let x = -1; x <= 1; x++) {
-          for (let y = -1; y <= 1; y++) {
-            if (row + x >= 0 && row + x < height && col + y >= 0 && col + y < width && nextSquares[row+x][col+y][0] === "Hidden") {
-              nextSquares = revealSquare(nextSquares, row + x, col + y);
+  function clickSquare (currentSquares: Array<Array<SquareType>>, row: number, col: number, clickType: string) {
+    
+    let nextSquares = currentSquares.slice();
+    const isFlag = clickType==="LEFT" && flagMode || clickType==="RIGHT" && !flagMode;
+    if (isFlag) {
+      if (nextSquares[row][col][0] === "Hidden") {
+        nextSquares[row][col][0] = "Flagged";
+      } else if (nextSquares[row][col][0] === "Flagged") {
+        nextSquares[row][col][0] = "Hidden";
+      }
+    } else {
+      if (nextSquares[row][col][0] === "Hidden") {
+        squaresRevealedCounter++;
+        nextSquares[row][col][0] = "Revealed";
+        setSquaresRevealed(squaresRevealed + 1);
+        if (nextSquares[row][col][2] === 0) {
+          for (let x = -1; x <= 1; x++) {
+            for (let y = -1; y <= 1; y++) {
+              if (row + x >= 0 && row + x < height && col + y >= 0 && col + y < width && nextSquares[row+x][col+y][0] === "Hidden") {
+                nextSquares = clickSquare(nextSquares, row + x, col + y, clickType);
+              }
             }
           }
+        }
+        if (nextSquares[row][col][1]) {
+          setGameIsOver(true);
+          setGameStatus("You lose!");
         }
       }
     }
     return nextSquares;
   }
 
-  const handleClick = (row: number, col: number) => {
-    const nextSquares = revealSquare(squares.slice(), row, col);
-    setSquares(nextSquares);
+  const handleClick = (row: number, col: number, clickType: string) => {
+    if (!gameIsOver) {
+      squaresRevealedCounter = 0;
+      const nextSquares = clickSquare(squares.slice(), row, col, clickType);
+      setSquares(nextSquares);
+      if (squaresRevealed + squaresRevealedCounter === width * height - numBombs) {
+        setGameIsOver(true);
+        setGameStatus("You win!");
+      }
+      setSquaresRevealed(squaresRevealed + squaresRevealedCounter);
+    }
   }
-  
 
-  function renderBoard() {
-    return (
-      <>
-      {rows.map((row) => (
-        <div className="board-row" key={row}>
-          {cols.map((col) => {
-            const index = row * width + col;
-            return (<Square 
-              key = {index}
-              state={squares[row][col][0]} 
-              isBomb={squares[row][col][1]}
-              value={squares[row][col][2]}
-              onSquareClick={() => handleClick(row, col)}
-              />);
-        })}
-        </div>
-      ))}
-      </>
-    )
+  const handleLeftClick = (row: number, col: number) => {
+    handleClick(row, col, "LEFT");
   }
-   
+
+  const handleRightClick = (e: React.SyntheticEvent, row: number, col: number) => {
+    e.preventDefault();
+    handleClick(row, col, "RIGHT");
+  }
+
+  const restartBoard = () => {
+    const nextSquares = generateBombs(initialSquares, numBombs);
+    setSquares(nextSquares);
+    setGameIsOver(false);
+    setGameStatus("");
+    setFlagMode(false);
+    setSquaresRevealed(0);
+  }
+
+  const invertFlagMode = () => {
+    setFlagMode(!flagMode);
+  }
+
+  function renderFlagModeButton () {
+    if (flagMode) {
+      return <button onClick={invertFlagMode} className="flag-mode-on">Flag Mode</button>;
+    } else {
+      return <button onClick={invertFlagMode} className="flag-mode-off">Flag Mode</button>;
+    }
+    
+  }
 
   return (
     <>
-    {renderBoard()}
+    <div className="game">
+      <div className="title">
+        <h3>Minesweeper!</h3>
+        <h3>{gameStatus}</h3>
+        <button onClick={restartBoard}>Restart</button>
+        {renderFlagModeButton()}
+      </div>
+      <div className="game-board">
+        <Board
+        width={width}
+        height={height}
+        numBombs={numBombs}
+        squares={squares}
+        handleLeftClick={handleLeftClick}
+        handleRightClick={handleRightClick}
+        />
+      </div>
+    </div>
     </>
   );
 }
